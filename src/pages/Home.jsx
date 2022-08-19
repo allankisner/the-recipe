62f98dbda681dd5500e44a1f9abddd101413f8af
Allan Kisner <kisner.dev@gmail.com>
2022-08-19 02:12:09 -0300

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Veggie from "../components/Veggie";
+import Popular from "../components/Popular";
+import Cocktail from "../components/Cocktail";
+
+function Home() {
+  return (
+    <>
+    <Popular />
+    <Veggie />
+    <Cocktail />
+    </>
+  )
+}
+
+export default Home
